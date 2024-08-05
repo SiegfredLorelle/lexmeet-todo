36a7c743f7a7b605d2daae667d706539e8048a57
SiegfredLorelle <siegfredlorelle09@gmail.com>
2024-08-05 11:08:07 +0800

    Create sidebar component and display it in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import './components/sidebar'
+import SideBar from './components/sidebar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <SideBar />
+
+
+
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
