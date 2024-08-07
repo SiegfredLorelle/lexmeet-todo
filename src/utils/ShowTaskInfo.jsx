0d104e14a5de182125f6d6747b018f4c8c136270
SiegfredLorelle <siegfredlorelle09@gmail.com>
2024-08-08 03:55:56 +0800

    Separate show task info function in a diff file

---
+++ src/utils/ShowTaskInfo.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import TaskInfoModal from "../components/TaskInfoModal";
+
+const ShowTaskInfo = ({task, children}) => {
+  const [isInfoTaskModalOpen, setIsInfoTaskModalOpen] = useState(false);
+
+  const openInfoTaskModal = (task) => {
+    setIsInfoTaskModalOpen(true);
+    setSelectedTask(task);
+  };
+
+  const closeInfoTaskModal = () => {
+    setIsInfoTaskModalOpen(false);
+    setSelectedTask(null);
+  };
+
+  return (
+    <>
+      <button onClick={(e) => { e.stopPropagation(); openInfoTaskModal(); }}>{children}</button>
+      <TaskInfoModal
+        isOpen={isInfoTaskModalOpen}
+        onClose={closeInfoTaskModal}
+        task={task}
+      />
+    </>
+  )
+};
+
+export default ShowTaskInfo;
