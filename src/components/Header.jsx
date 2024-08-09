9d92c502833b12d6875db0321a9be741bd325d77
SiegfredLorelle <siegfredlorelle09@gmail.com>
2024-08-09 13:29:20 +0800

    Add linked in and github profile in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Header = ({ toggleListsSection }) => {
@@ -13,6 +14,14 @@ const Header = ({ toggleListsSection }) => {
           <a href="/" className="header-link">
             <img src="logo.png" alt="Logo" />
             <h1 className="header-title">ToDo DuDu</h1>
+          </a>
+        </div>
+        <div className="header-right">
+          <a href="https://github.com/SiegfredLorelle" target="_blank" rel="noopener noreferrer" className="header-icon">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href="https://www.linkedin.com/in/siegfred-lorelle-mina" target="_blank" rel="noopener noreferrer" className="header-icon">
+            <FontAwesomeIcon icon={faLinkedinIn} />
           </a>
         </div>
       </header>
